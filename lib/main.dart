318716532ba20da6ffa3caff99fab3c69869ded0
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2020-01-19 19:42:38 -0300

    Commit inicial - Section 11: Navegando entre telas

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:navegacao/TelaSecundaria.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.pink,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaSecundaria() ));
+              },
+              color: Colors.blue,
+              textColor: Colors.white,
+              child: Text(
+                  "Ir para segunda tela"
+              ,style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold
+              ),),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
